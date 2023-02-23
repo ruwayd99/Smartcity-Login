5a88ba7ad0564e253aa580d70f1ec02faf26b648
Mir Ruwayd Afeef <ruwaydafeef99@gmail.com>
2023-02-23 09:45:12 -0800

    first commit

---
+++ user/models.py
@@ -0,0 +1,50 @@
+from flask import Flask, jsonify, request, session, redirect
+from passlib.hash import pbkdf2_sha256
+from app import db
+import uuid
+
+class User:
+
+  def start_session(self, user):
+    del user['password']
+    session['logged_in'] = True
+    session['user'] = user
+    return jsonify(user), 200
+
+  def signup(self):
+    print(request.form)
+
+    # Create the user object
+    user = {
+      "_id": uuid.uuid4().hex,
+      "name": request.form.get('name'),
+      "student_no": request.form.get('student_no'),
+      "password": request.form.get('password')
+    }
+
+    # Encrypt the password
+    user['password'] = pbkdf2_sha256.encrypt(user['password'])
+
+    # Check for existing student_no address
+    if db.users.find_one({ "student_no": user['student_no'] }):
+      return jsonify({ "error": "Student number already in use" }), 400
+
+    if db.users.insert_one(user):
+      return self.start_session(user)
+
+    return jsonify({ "error": "Signup failed" }), 400
+  
+  def signout(self):
+    session.clear()
+    return redirect('/')
+  
+  def login(self):
+
+    user = db.users.find_one({
+      "student_no": request.form.get('student_no')
+    })
+
+    if user and pbkdf2_sha256.verify(request.form.get('password'), user['password']):
+      return self.start_session(user)
+    
+    return jsonify({ "error": "Invalid login credentials" }), 401
